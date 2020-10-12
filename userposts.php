faa1517e0f609a371d6d6c8be91534406ed033b3
Abreu <ramos.paulo1989@gmail.com>
2020-10-12 23:09:21 +0100

    Capitalized title

--- userposts.php
+++ userposts.php
@@ -22,7 +22,7 @@ if (isset($_GET['logout'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>User posts</title>
+    <title>User Posts</title>
 </head>
 
 <body>
