20f39a9348ed2fedf10cc3e66caef0352f10edad
Abreu <ramos.paulo1989@gmail.com>
2020-10-13 00:01:58 +0100

    Using AJAX to get user posts - Rendering posts from the database is now handled by postshtml.php - Added javascript to fetch user posts on button click

---
+++ postshtml.php
@@ -0,0 +1,43 @@
+<?php
+
+include('db.php');
+
+
+if (isset($_GET['user_id'])) {
+
+    $res_users = mysqli_query($db, "SELECT * FROM registered_users WHERE id=" . mysqli_real_escape_string($db, $_GET['user_id']));
+    $found_user = mysqli_fetch_assoc($res_users);
+
+    if (isset($found_user)) {
+?>
+        <h2>Posts from <?= $found_user['username'] ?></h2>
+        <br>
+        <br>
+        <hr>
+        <?php
+        /* $stmt = mysqli_stmt_init($db);
+                    $stmt = mysqli_stmt_prepare($stmt, "SELECT * FROM to_do WHERE user_id=?");
+                    $stmt = mysqli_stmt_bind_param($stmt, "i", $_GET["user_id"]);
+                    $stmt = mysqli_stmt_execute($stmt);
+                    $stmt = mysqli_stmt_bind_result($stmt, $) */
+
+        $res = mysqli_query($db, "SELECT * FROM to_do WHERE user_id='" . mysqli_real_escape_string($db, $_GET['user_id']) . "'");
+        $posts = mysqli_fetch_all($res, MYSQLI_ASSOC);
+
+        foreach ($posts as $p) { ?>
+            <div>
+                <p class="mainPost"> <?= $p['text'] ?> </p>
+                <br>
+                <p> <?= $found_user['username'] . ", " . $p['date'] ?> </p>
+                <br>
+            </div>
+            <hr>
+        <?php
+        }
+    } else {
+        ?>
+        <h2>No user found</h2>
+<?php
+    }
+}
+?>
--- userposts.php
+++ userposts.php
@@ -40,7 +40,7 @@ if (isset($_GET['logout'])) {
                 <?php
                 foreach ($users as $user) {
                     if ($user['username'] != $_SESSION['username']) : ?>
-                        <li><button name="user_id" value="<?= $user['id'] ?>" class="userBut"><?= $user['username'] ?></button></li>
+                        <li><button name="user_id" value="<?= $user['id'] ?>" class="user-button"><?= $user['username'] ?></button></li>
                 <?php endif;
                 }
                 ?>
@@ -48,51 +48,28 @@ if (isset($_GET['logout'])) {
             </ul>
         </form>
         <div class="postArea">
-            <?php
-
-            if (isset($_GET['user_id'])) {
-                foreach ($users as $u) {
-                    if ($u['id'] === $_GET['user_id']) {
-                        $found_user = $u;
-                        break;
-                    }
-                }
-
-                if (isset($found_user)) {
-            ?>
-                    <h2>Posts from <?= $u['username'] ?></h2>
-                    <br>
-                    <br>
-                    <hr>
-                    <?php
-                    /* $stmt = mysqli_stmt_init($db);
-                    $stmt = mysqli_stmt_prepare($stmt, "SELECT * FROM to_do WHERE user_id=?");
-                    $stmt = mysqli_stmt_bind_param($stmt, "i", $_GET["user_id"]);
-                    $stmt = mysqli_stmt_execute($stmt);
-                    $stmt = mysqli_stmt_bind_result($stmt, $) */
-
-                    $res = mysqli_query($db, "SELECT * FROM to_do WHERE user_id='" . mysqli_real_escape_string($db, $_GET['user_id']) . "'");
-                    $posts = mysqli_fetch_all($res, MYSQLI_ASSOC);
-
-                    foreach ($posts as $p) { ?>
-                        <div>
-                            <p class="mainPost"> <?= $p['text'] ?> </p>
-                            <br>
-                            <p> <?= $u['username'] . ", " . $p['date'] ?> </p>
-                            <br>
-                        </div>
-                        <hr>
-                    <?php
-                    }
-                } else {
-                    ?>
-                    <h2>No user found</h2>
-            <?php
-                }
-            }
-            ?>
         </div>
     </div>
+    <script>
+        const buttons = document.querySelectorAll('.user-button');
+        const postArea = document.querySelector('.postArea');
+
+        function handleClick(e) {
+            e.preventDefault();
+
+            const userId = e.target.value;
+
+            fetch("/registration/postshtml.php?user_id=" + userId)
+                .then(res => res.text())
+                .then(html => {
+                    postArea.innerHTML = html
+                });
+        }
+
+        buttons.forEach(button => {
+            button.addEventListener('click', handleClick);
+        })
+    </script>
 </body>
 
 </html>
